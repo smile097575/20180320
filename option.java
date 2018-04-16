1018e635024fc9b5a4544aefbda62ed753565379
smile097575 <smile097575@gmail.com>
2018-04-16 23:21:24 +0800

    Create option.java

---
+++ option.java
@@ -0,0 +1,9 @@
+public class option {
+    public void options(){
+        System.out.println("1查詢某位同學");
+        System.out.println("2顯示出全部同學資料");
+        System.out.println("3查詢科目平均");
+        System.out.println("4查詢某同學的總分與平均");
+        System.out.println("5結束");
+    }
+}
