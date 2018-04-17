a903be8ef3c1c5e9bcbb8957bb86d2bf3b5e9075
smile097575 <smile097575@gmail.com>
2018-04-17 11:06:51 +0800

    Update option.java

--- option.java
+++ option.java
@@ -1,9 +1,9 @@
 public class option {
     public void options(){
-        System.out.println("1查詢某位同學");
-        System.out.println("2顯示出全部同學資料");
-        System.out.println("3查詢科目平均");
-        System.out.println("4查詢某同學的總分與平均");
-        System.out.println("5結束");
+        System.out.println("查詢學生");
+        System.out.println("顯示出全部同學資料");
+        System.out.println("查詢科目平均");
+        System.out.println("查詢學生的總分與平均");
+        System.out.println("END");
     }
 }
